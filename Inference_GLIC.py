a9c5227588e3848b317e0966f3cff413e0ba6631
DuBianJun-007 <mingshengzhou@foxmail.com>
2024-05-18 17:43:06 +0800

    update

--- Inference_GLIC.py
+++ Inference_GLIC.py
@@ -157,7 +157,7 @@ def setup_args() -> argparse.ArgumentParser:
         "-p", "--path",
         dest="paths",
         type=str,
-        default='./model/GLIC-0.0075.pth.tar',
+        default='model/GLIC_8_0.09.pth.tar',
         help="Model path."
     )
     parser.add_argument(
